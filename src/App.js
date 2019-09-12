bfb5fc9b8e102ba71eb151e6fb9fa92aeb122374
mukeunzi <eunji057@gmail.com>
2019-09-12 16:58:59 +0900

    feat : props.children 실습

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from 'react';
 import Hello from './Hello';
+import Wrapper from './Wrapper';
 import './App.css';
 
 function App() {
-	const name = '묵묵';
-	const style = {
-		backgroundColor: 'red',
-		color: 'yellow',
-		fontSize: 24
-	};
-
 	return (
-		<>
-			<div>
-				<Hello />
-				<Hello />
-				<br></br>
-				<Hello />
-			</div>
-
-			<div style={style}>{name}</div>
-			<div
-				className="muk-box" //안보여??
-			></div>
-			{/* 안보일 것인가!!! */}
-
+		<Wrapper>
 			<Hello name="묵이당" color="red" />
 			<Hello color="purple" />
-		</>
+		</Wrapper>
 	);
 }
 
