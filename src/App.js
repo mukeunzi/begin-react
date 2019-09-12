301ce4e1b5a8eeebdd1b93c5a4b0f86c7ff3e666
mukeunzi <eunji057@gmail.com>
2019-09-12 16:51:00 +0900

    feat : props 기본 문법 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+	const name = '묵묵';
+	const style = {
+		backgroundColor: 'red',
+		color: 'yellow',
+		fontSize: 24
+	};
+
 	return (
-		<div>
-			<Hello />
-			<Hello />
-			<Hello />
-		</div>
+		<>
+			<div>
+				<Hello />
+				<Hello />
+				<br></br>
+				<Hello />
+			</div>
+
+			<div style={style}>{name}</div>
+			<div
+				className="muk-box" //안보여??
+			></div>
+			{/* 안보일 것인가!!! */}
+
+			<Hello name="묵이당" color="red" />
+			<Hello color="purple" />
+		</>
 	);
 }
 
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function Hello() {
-	return <div>안녕하세요</div>;
+function Hello({ color, name }) {
+	return <div style={{ color: color }}>안뇽 {name}</div>;
 }
+
+Hello.defaultProps = {
+	name: '디폴트 묵!'
+};
 
 export default Hello;
