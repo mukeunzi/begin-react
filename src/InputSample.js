6d356e31a38878d8c4d1335689acb4a47d1e8b34
mukeunzi <eunji057@gmail.com>
2019-09-14 17:20:23 +0900

    feat : 특정 DOM을 선택하는 useRef 실습
    - DOM의 ref 값으로 설정한 값을 .current 로 선택할 수 있음

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
 	const [inputs, setInputs] = useState({ name: '', nickname: '' });
+	const nameInput = useRef();
 
 	const { name, nickname } = inputs;
 
@@ -15,11 +16,12 @@ function InputSample() {
 
 	const onReset = () => {
 		setInputs({ name: '', nickname: '' });
+		nameInput.current.focus();
 	};
 
 	return (
 		<div>
-			<input name="name" placeholder="이름" onChange={onChange} value={name} />
+			<input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput} />
 			<input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
 			<button onClick={onReset}>초기화</button>
 
